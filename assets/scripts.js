ed21e72039844be9ee15fe051df93b9427014da1
Nana Boateng <nanakab.dev@gmail.com>
2026-02-26 06:42:30 +0000

    added contribution logic

--- assets/scripts.js
+++ assets/scripts.js
@@ -3,6 +3,10 @@ const addMemberModal = document.querySelector(".addMemberModal");
 const modalCancel = document.querySelector(".modalCancelBtn");
 const modalAddMember = document.querySelector(".modalAddMemberBtn");
 
+const addContributionBtn = document.querySelector(".addContributionBtn");
+const addContributionModal = document.querySelector(".addContributionModal");
+const submitContributionBtn = document.querySelector(".submitContributionBtn");
+
 
 
 addMemberBtns.forEach((addMemberBtn) =>{
@@ -13,15 +17,27 @@ addMemberBtn.addEventListener("click", () =>{
 })
 })
 
-modalCancel.addEventListener("click", ()=>{
+modalCancel.addEventListener("click", (event)=>{
+    event.preventDefault();
     addMemberModal.close();
 })
 
-modalAddMember.addEventListener("click", () =>{
-   
+modalAddMember.addEventListener("click", (event) =>{
+   event.preventDefault();
     alert("thank you!!");
 
       addMemberModal.close();
 })
 
 
+addContributionBtn.addEventListener("click", () => {
+    addContributionModal.showModal();
+})
+
+
+submitContributionBtn.addEventListener("click", (event) =>{
+    event.preventDefault();
+    // add it to the records 
+    // show added message
+    addContributionModal.close();
+})
